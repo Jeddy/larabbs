ebf19e6925505e33ac7d6c15ebed6924fe44aea0
Jeddy <jeddyliu@foxmail.com>
2018-10-19 13:53:10 +0000

    Admin can manage content

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -65,7 +65,7 @@
 
             {{-- 用户回复列表 --}}
             <div class="panel panel-default topic-reply">
-                <div class="panel-body">
+                <div class="panel-body">s
                     @includeWhen(Auth::check(), 'topics._reply_box', ['topic' => $topic])
                     @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
                 </div>
