15d1c0863ad7ece149f474d7a2d02cbbbc07abb9
Jeddy <jeddyliu@foxmail.com>
2018-11-17 09:03:00 +0000

    user update

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\models\User;
+use App\models\Image;
 use Illuminate\Http\Request;
 use App\Http\Requests\Api\UserRequest;
 use App\Transformers\UserTransformer;
@@ -42,5 +43,21 @@ class UsersController extends Controller
     {
         return $this->response->item($this->user(), new UserTransformer())->setStatusCode(201);
     }
+
+    public function update(UserRequest $request)
+	{
+	    $user = $this->user();
+
+	    $attributes = $request->only(['name', 'email', 'introduction']);
+
+	    if ($request->avatar_image_id) {
+	        $image = Image::find($request->avatar_image_id);
+
+	        $attributes['avatar'] = $image->path;
+	    }
+	    $user->update($attributes);
+
+	    return $this->response->item($user, new UserTransformer());
+	}
 
 }
